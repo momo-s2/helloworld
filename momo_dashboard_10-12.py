03d5427b67dc9d9267ffbf54f661ffdd39ed396e
Momoka Suzuki <75671810+momo-s2@users.noreply.github.com>
2022-08-23 07:36:26 +0900

    delete unnecessary line
    delete unnecessary line

--- momo_dashboard_10-12.py
+++ momo_dashboard_10-12.py
@@ -33,15 +33,6 @@ options = st.multiselect(
 
 st.write('You selected:',options)
 
-st.header('st.multiselect')
-
-options = st.multiselect(
-     'What are your favorite colors',
-     ['Green', 'Yellow', 'Red', 'Blue'],
-     ['Yellow', 'Red'])
-
-st.write('You selected:', options)
-
 st.subheader('st.checkbox')
 
 st.write('which country would you like to go?')
