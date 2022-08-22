f59137e3fcb70a1ce16fd3c36e57f54d9498596d
Momoka Suzuki <75671810+momo-s2@users.noreply.github.com>
2022-08-23 07:30:28 +0900

    Add files via upload

---
+++ momo_dashboard_10-12.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 20 15:09:44 2022
+
+@author: momo
+"""
+#import libs
+import numpy as np
+import altair as alt
+import pandas as pd
+import streamlit as st
+from datetime import time, datetime
+
+st.title('#30 Days of Streamlit')
+st.header('Day 10-12 CHALLENGE:hatched_chick:')
+
+st.subheader('st.selectbox')
+
+option = st.selectbox(
+    'which area would you like to go?',
+    ('Africa','America','Antarctica','Asia','Europe','Oceania'))
+
+st.write('Where you want to go is', option)
+
+
+st.subheader('st.multiselect')
+
+options = st.multiselect(
+    'which country would you like to go?',
+    ['Kenya','Uganda','Tanzania','Rwanda','Brundi','Somalia'],
+    )
+
+st.write('You selected:',options)
+
+st.header('st.multiselect')
+
+options = st.multiselect(
+     'What are your favorite colors',
+     ['Green', 'Yellow', 'Red', 'Blue'],
+     ['Yellow', 'Red'])
+
+st.write('You selected:', options)
+
+st.subheader('st.checkbox')
+
+st.write('which country would you like to go?')
+
+Kenya = st.checkbox('Kenya')
+Tanzania = st.checkbox('Tanzania')
+Uganda = st.checkbox('Uganda')
+
+if Kenya:
+    st.write('You should go Masai Mara 🐘')
+if Tanzania:
+    st.write('You should go Zanzibar 🌴')
+if Uganda:
+    st.write('You should go Bwindi Impenetrable National Park 🦍')
