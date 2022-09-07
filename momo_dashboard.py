71aa0e2c031bd0cd7c43f7d8eab15db2ccccf150
Momoka Suzuki <75671810+momo-s2@users.noreply.github.com>
2022-09-07 19:42:28 +0900

    day29
    day29

--- momo_dashboard.py
+++ momo_dashboard.py
@@ -1,70 +1,535 @@
+# Import Streamlit and Pandas
 import streamlit as st
-from streamlit_shap import st_shap
-import shap
-from sklearn.model_selection import train_test_split
-import xgboost
-import numpy as np
 import pandas as pd
 
-st.set_page_config(layout="wide")
+# Import for API calls
+import requests
 
-@st.experimental_memo
-def load_data():
-    return shap.datasets.adult()
+# Import for navbar
+from streamlit_option_menu import option_menu
 
-@st.experimental_memo
-def load_model(X, y):
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=7)
-    d_train = xgboost.DMatrix(X_train, label=y_train)
-    d_test = xgboost.DMatrix(X_test, label=y_test)
-    params = {
-        "eta": 0.01,
-        "objective": "binary:logistic",
-        "subsample": 0.5,
-        "base_score": np.mean(y_train),
-        "eval_metric": "logloss",
-        "n_jobs": -1,
-    }
-    model = xgboost.train(params, d_train, 10, evals = [(d_test, "test")], verbose_eval=100, early_stopping_rounds=20)
-    return model
+# Import for dyanmic tagging
+from streamlit_tags import st_tags, st_tags_sidebar
 
-st.title("`streamlit-shap` for displaying SHAP plots in a Streamlit app")
+# Imports for aggrid
+from st_aggrid import AgGrid
+from st_aggrid import AgGrid
+import pandas as pd
+from st_aggrid.grid_options_builder import GridOptionsBuilder
+from st_aggrid.shared import JsCode
+from st_aggrid import GridUpdateMode, DataReturnMode
 
-with st.expander('About the app'):
-    st.markdown('''[`streamlit-shap`](https://github.com/snehankekre/streamlit-shap) is a Streamlit component that provides a wrapper to display [SHAP](https://github.com/slundberg/shap) plots in [Streamlit](https://streamlit.io/). 
-                    The library is developed by our in-house staff [Snehan Kekre](https://github.com/snehankekre) who also maintains the [Streamlit Documentation](https://docs.streamlit.io/) website.
-                ''')
+# Import for keyboard shortcuts
+#from dashboard_utils.gui import keyboard_to_url
+#from dashboard_utils.gui import load_keyboard_class
 
-st.header('Input data')
-X,y = load_data()
-X_display,y_display = shap.datasets.adult(display=True)
+#######################################################
 
-with st.expander('About the data'):
-    st.write('Adult census data is used as the example dataset.')
-with st.expander('X'):
-    st.dataframe(X)
-with st.expander('y'):
-    st.dataframe(y)
+# The code below is to control the layout width of the app.
+if "widen" not in st.session_state:
+    layout = "centered"
+else:
+    layout = "wide" if st.session_state.widen else "centered"
 
-st.header('SHAP output')
+#######################################################
 
-# train XGBoost model
-model = load_model(X, y)
+# The code below is for the title and logo.
+st.set_page_config(layout=layout, page_title="Zero-Shot Text Classifier", page_icon="🤗")
 
-# compute SHAP values
-explainer = shap.Explainer(model, X)
-shap_values = explainer(X)
+#######################################################
 
-with st.expander('Waterfall plot'):
-    st_shap(shap.plots.waterfall(shap_values[0]), height=300)
-with st.expander('Beeswarm plot'):
-    st_shap(shap.plots.beeswarm(shap_values), height=300)
+# The class below is for adding some formatting to the shortcut button on the left sidebar.
+#load_keyboard_class()
 
-explainer = shap.TreeExplainer(model)
-shap_values = explainer.shap_values(X)
+#######################################################
 
-with st.expander('Force plot'):
-    st.subheader('First data instance')
-    st_shap(shap.force_plot(explainer.expected_value, shap_values[0,:], X_display.iloc[0,:]), height=200, width=1000)
-    st.subheader('First thousand data instance')
-    st_shap(shap.force_plot(explainer.expected_value, shap_values[:1000,:], X_display.iloc[:1000,:]), height=400, width=1000)
+# Set up session state so app interactions don't reset the app.
+if not "valid_inputs_received" in st.session_state:
+    st.session_state["valid_inputs_received"] = False
+
+#######################################################
+
+# The block of code below is to display the title, logos and introduce the app.
+
+c1, c2 = st.columns([0.4, 2])
+
+with c1:
+
+    st.image(
+        "logo.png",
+        width=110,
+    )
+
+with c2:
+
+    st.caption("")
+    st.title("Zero-Shot Text Classifier")
+
+
+st.sidebar.image(
+    "logo.png",
+)
+
+st.write("")
+
+st.markdown(
+    """
+Classify keyphrases fast and on-the-fly with this mighty app. No ML training needed!
+Create classifying labels (e.g. `Positive`, `Negative` and `Neutral`), paste your keyphrases, and you're off!  
+"""
+)
+
+st.write("")
+
+st.sidebar.write("")
+
+#######################################################
+
+# The code below is to display the menu bar.ß
+with st.sidebar:
+    selected = option_menu(
+        "",
+        ["Demo (5 phrases max)", "Unlocked Mode"],
+        icons=["bi-joystick", "bi-key-fill"],
+        menu_icon="",
+        default_index=0,
+    )
+
+#######################################################
+
+# The code below is to display the shortcuts.
+st.sidebar.header("Shortcuts")
+st.sidebar.write(
+    '<span class="kbdx">G</span>  &nbsp; GitHub',
+    unsafe_allow_html=True,
+)
+
+st.sidebar.write(
+    '<span class="kbdx">&thinsp;.&thinsp;</span>  &nbsp; GitHub Dev (VS Code)',
+    unsafe_allow_html=True,
+)
+
+#######################################################
+
+# The block of code below is to display information about Streamlit.
+
+st.sidebar.markdown("---")
+
+# Sidebar
+st.sidebar.header("About")
+
+st.sidebar.markdown(
+    """
+App created by [Datachaz](https://twitter.com/DataChaz) using 🎈[Streamlit](https://streamlit.io/) and [HuggingFace](https://huggingface.co/inference-api)'s [Distilbart-mnli-12-3](https://huggingface.co/valhalla/distilbart-mnli-12-3) model.
+"""
+)
+
+st.sidebar.markdown(
+    "[Streamlit](https://streamlit.io) is a Python library that allows the creation of interactive, data-driven web applications in Python."
+)
+
+st.sidebar.header("Resources")
+st.sidebar.markdown(
+    """
+- [Streamlit Documentation](https://docs.streamlit.io/)
+- [Cheat sheet](https://docs.streamlit.io/library/cheatsheet)
+- [Book](https://www.amazon.com/dp/180056550X) (Getting Started with Streamlit for Data Science)
+- [Blog](https://blog.streamlit.io/how-to-master-streamlit-for-data-science/) (How to master Streamlit for data science)
+"""
+)
+
+st.sidebar.header("Deploy")
+st.sidebar.markdown(
+    "You can quickly deploy Streamlit apps using [Streamlit Cloud](https://streamlit.io/cloud) in just a few clicks."
+)
+
+
+def main():
+    st.caption("")
+
+
+if selected == "Demo (5 phrases max)":
+
+    API_KEY = st.secrets["API_KEY"]
+
+    API_URL = (
+        "https://api-inference.huggingface.co/models/valhalla/distilbart-mnli-12-3"
+    )
+
+    headers = {"Authorization": f"Bearer {API_KEY}"}
+
+    with st.form(key="my_form"):
+
+        multiselectComponent = st_tags(
+            label="",
+            text="Add labels - 3 max",
+            value=["Transactional", "Informational", "Navigational"],
+            suggestions=[
+                "Informational",
+                "Transactional",
+                "Navigational",
+                "Positive",
+                "Negative",
+                "Neutral",
+            ],
+            maxtags=3,
+        )
+
+        new_line = "\n"
+        nums = [
+            "I want to buy something in this store",
+            "How to ask a question about a product",
+            "Request a refund through the Google Play store",
+            "I have a broken screen, what should I do?",
+            "Can I have the link to the product?",
+        ]
+
+        sample = f"{new_line.join(map(str, nums))}"
+
+        linesDeduped2 = []
+
+        MAX_LINES = 5
+        text = st.text_area(
+            "Enter keyphrases to classify",
+            sample,
+            height=200,
+            key="2",
+            help="At least two keyphrases for the classifier to work, one per line, "
+            + str(MAX_LINES)
+            + " keyphrases max as part of the demo",
+        )
+        lines = text.split("\n")  # A list of lines
+        linesList = []
+        for x in lines:
+            linesList.append(x)
+        linesList = list(dict.fromkeys(linesList))  # Remove dupes
+        linesList = list(filter(None, linesList))  # Remove empty
+
+        if len(linesList) > MAX_LINES:
+
+            st.info(
+                f"❄️  Only the first "
+                + str(MAX_LINES)
+                + " keyprases will be reviewed. Unlock that limit by switching to 'Unlocked Mode'"
+            )
+
+        linesList = linesList[:MAX_LINES]
+
+        submit_button = st.form_submit_button(label="Submit")
+
+    if not submit_button and not st.session_state.valid_inputs_received:
+        st.stop()
+
+    elif submit_button and not text:
+        st.warning("❄️ There is no keyphrases to classify")
+        st.session_state.valid_inputs_received = False
+        st.stop()
+
+    elif submit_button and not multiselectComponent:
+        st.warning("❄️ You have not added any labels, please add some! ")
+        st.session_state.valid_inputs_received = False
+        st.stop()
+
+    elif submit_button and len(multiselectComponent) == 1:
+        st.warning("❄️ Please make sure to add at least two labels for classification")
+        st.session_state.valid_inputs_received = False
+        st.stop()
+
+    elif submit_button or st.session_state.valid_inputs_received:
+
+        if submit_button:
+            st.session_state.valid_inputs_received = True
+
+        def query(payload):
+            response = requests.post(API_URL, headers=headers, json=payload)
+            # Unhash to check status codes from the API response
+            # st.write(response.status_code)
+            return response.json()
+
+        listtest = ["I want a refund", "I have a question"]
+        listToAppend = []
+
+        for row in linesList:
+            output2 = query(
+                {
+                    "inputs": row,
+                    "parameters": {"candidate_labels": multiselectComponent},
+                    "options": {"wait_for_model": True},
+                }
+            )
+
+            listToAppend.append(output2)
+
+            df = pd.DataFrame.from_dict(output2)
+
+        st.success("✅ Done!")
+
+        df = pd.DataFrame.from_dict(listToAppend)
+
+        st.caption("")
+        st.markdown("### Check classifier results")
+        st.caption("")
+
+        st.checkbox(
+            "Widen layout",
+            key="widen",
+            help="Tick this box to toggle the layout to 'Wide' mode",
+        )
+
+        st.caption("")
+
+        # This is a list comprehension to convert the decimals to percentages
+        f = [[f"{x:.2%}" for x in row] for row in df["scores"]]
+
+        # This code is for re-integrating the labels back into the dataframe
+        df["classification scores"] = f
+        df.drop("scores", inplace=True, axis=1)
+
+        # This code is to rename the columns
+        df.rename(columns={"sequence": "keyphrase"}, inplace=True)
+
+        # The code below is for Ag-grid
+
+        gb = GridOptionsBuilder.from_dataframe(df)
+        # enables pivoting on all columns
+        gb.configure_default_column(
+            enablePivot=True, enableValue=True, enableRowGroup=True
+        )
+        gb.configure_selection(selection_mode="multiple", use_checkbox=True)
+        gb.configure_side_bar()
+        gridOptions = gb.build()
+
+        response = AgGrid(
+            df,
+            gridOptions=gridOptions,
+            enable_enterprise_modules=True,
+            update_mode=GridUpdateMode.MODEL_CHANGED,
+            data_return_mode=DataReturnMode.FILTERED_AND_SORTED,
+            height=300,
+            fit_columns_on_grid_load=False,
+            configure_side_bar=True,
+        )
+
+        # The code below is for the download button
+
+        cs, c1 = st.columns([2, 2])
+
+        with cs:
+
+            @st.cache
+            def convert_df(df):
+                # IMPORTANT: Cache the conversion to prevent computation on every rerun
+                return df.to_csv().encode("utf-8")
+
+            csv = convert_df(df)  #
+
+            st.download_button(
+                label="Download results as CSV",
+                data=csv,
+                file_name="results.csv",
+                mime="text/csv",
+            )
+
+elif selected == "Unlocked Mode":
+
+    with st.form(key="my_form"):
+        API_KEY2 = st.text_input(
+            "Enter your 🤗 HuggingFace API key",
+            help="Once you created you HuggiginFace account, you can get your free API token in your settings page: https://huggingface.co/settings/tokens",
+        )
+
+        API_URL = (
+            "https://api-inference.huggingface.co/models/valhalla/distilbart-mnli-12-3"
+        )
+
+        headers = {"Authorization": f"Bearer {API_KEY2}"}
+
+        multiselectComponent = st_tags(
+            label="",
+            text="Add labels - 3 max",
+            value=["Transactional", "Informational", "Navigational"],
+            suggestions=[
+                "Informational",
+                "Transactional",
+                "Navigational",
+                "Positive",
+                "Negative",
+                "Neutral",
+            ],
+            maxtags=3,
+        )
+
+        new_line = "\n"
+        nums = [
+            "I want to buy something in this store",
+            "How to ask a question about a product",
+            "Request a refund through the Google Play store",
+            "I have a broken screen, what should I do?",
+            "Can I have the link to the product?",
+        ]
+
+        sample = f"{new_line.join(map(str, nums))}"
+
+        linesDeduped2 = []
+
+        MAX_LINES_FULL = 50
+        text = st.text_area(
+            "Enter keyphrases to classify",
+            sample,
+            height=200,
+            key="2",
+            help="At least two keyphrases for the classifier to work, one per line, "
+            + str(MAX_LINES_FULL)
+            + " keyphrases max in 'unlocked mode'. You can tweak 'MAX_LINES_FULL' in the code to change this",
+        )
+
+        lines = text.split("\n")  # A list of lines
+        linesList = []
+        for x in lines:
+            linesList.append(x)
+        linesList = list(dict.fromkeys(linesList))  # Remove dupes from list
+        linesList = list(filter(None, linesList))  # Remove empty lines from list
+
+        if len(linesList) > MAX_LINES_FULL:
+            st.info(
+                f"❄️ Note that only the first "
+                + str(MAX_LINES_FULL)
+                + " keyprases will be reviewed to preserve performance. Fork the repo and tweak 'MAX_LINES_FULL' in the code to increase that limit."
+            )
+
+            linesList = linesList[:MAX_LINES_FULL]
+
+        submit_button = st.form_submit_button(label="Submit")
+
+    if not submit_button and not st.session_state.valid_inputs_received:
+        st.stop()
+
+    elif submit_button and not text:
+        st.warning("❄️ There is no keyphrases to classify")
+        st.session_state.valid_inputs_received = False
+        st.stop()
+
+    elif submit_button and not multiselectComponent:
+        st.warning("❄️ You have not added any labels, please add some! ")
+        st.session_state.valid_inputs_received = False
+        st.stop()
+
+    elif submit_button and len(multiselectComponent) == 1:
+        st.warning("❄️ Please make sure to add at least two labels for classification")
+        st.session_state.valid_inputs_received = False
+        st.stop()
+
+    elif submit_button or st.session_state.valid_inputs_received:
+
+        try:
+
+            if submit_button:
+
+                st.session_state.valid_inputs_received = True
+
+            def query(payload):
+                response = requests.post(API_URL, headers=headers, json=payload)
+                # Unhash to check status codes from the API response
+                # st.write(response.status_code)
+                return response.json()
+
+            listtest = ["I want a refund", "I have a question"]
+            listToAppend = []
+
+            for row in linesList:
+                output2 = query(
+                    {
+                        "inputs": row,
+                        "parameters": {"candidate_labels": multiselectComponent},
+                        "options": {"wait_for_model": True},
+                    }
+                )
+
+                listToAppend.append(output2)
+
+                df = pd.DataFrame.from_dict(output2)
+
+            st.success("✅ Done!")
+
+            df = pd.DataFrame.from_dict(listToAppend)
+
+            st.caption("")
+            st.markdown("### Check classifier results")
+            st.caption("")
+
+            st.checkbox(
+                "Widen layout",
+                key="widen",
+                help="Tick this box to toggle the layout to 'Wide' mode",
+            )
+
+            # This is a list comprehension to convert the decimals to percentages
+            f = [[f"{x:.2%}" for x in row] for row in df["scores"]]
+
+            # This code is for re-integrating the labels back into the dataframe
+            df["classification scores"] = f
+            df.drop("scores", inplace=True, axis=1)
+
+            # This code is to rename the columns
+            df.rename(columns={"sequence": "keyphrase"}, inplace=True)
+
+            # The code below is for Ag-grid
+            gb = GridOptionsBuilder.from_dataframe(df)
+            # enables pivoting on all columns
+            gb.configure_default_column(
+                enablePivot=True, enableValue=True, enableRowGroup=True
+            )
+            gb.configure_selection(selection_mode="multiple", use_checkbox=True)
+            gb.configure_side_bar()
+            gridOptions = gb.build()
+
+            response = AgGrid(
+                df,
+                gridOptions=gridOptions,
+                enable_enterprise_modules=True,
+                update_mode=GridUpdateMode.MODEL_CHANGED,
+                data_return_mode=DataReturnMode.FILTERED_AND_SORTED,
+                height=300,
+                fit_columns_on_grid_load=False,
+                configure_side_bar=True,
+            )
+
+            # The code below is for the download button
+
+            cs, c1 = st.columns([2, 2])
+
+            with cs:
+
+                @st.cache
+                def convert_df(df):
+                    # IMPORTANT: Cache the conversion to prevent computation on every rerun
+                    return df.to_csv().encode("utf-8")
+
+                csv = convert_df(df)  #
+
+                st.caption("")
+
+                st.download_button(
+                    label="Download results as CSV",
+                    data=csv,
+                    file_name="results.csv",
+                    mime="text/csv",
+                )
+
+        except ValueError as ve:
+
+            st.warning("❄️ Add a valid HuggingFace API key in the text box above ☝️")
+            st.stop()
+
+
+if __name__ == "__main__":
+    main()
+
+keyboard_to_url(
+    key="g",
+    url="https://github.com/CharlyWargnier/zero-shot-classifier/blob/main/streamlit_app.py",
+)
+keyboard_to_url(
+    key_code=190,
+    url="https://github.dev/CharlyWargnier/zero-shot-classifier/blob/main/streamlit_app.py",
+)
